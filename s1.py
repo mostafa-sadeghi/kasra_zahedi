0ed62d062bcb8e2318fd5829f7fabeb94ca604e8
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-05 22:00:10 +0330

    sadeghi

---
+++ s1.py
@@ -0,0 +1,57 @@
+# primitive types in python
+# numbers
+# str
+# bool
+
+# x = 1
+# print(type(x))
+
+# x = 1.5
+# print(type(x))
+
+# x = 1 + 2j
+# print(type(x))
+
+# print("2+3*3")
+# print(2+3*3)
+
+# priority () **  * / + -
+
+# print((2+3)*3)
+
+# about str
+
+# x = 'abcd'
+# print(type(x))
+# x = "abcd"
+# print(type(x))
+
+# x = ''
+# print(type(x))
+# x = '1'
+# print(type(x))
+# x = 'a'
+# print(type(x))
+# x = ' '
+# print(type(x))
+
+
+x = '12345'
+print('length of string is:', len(x))
+
+# how to name a variable in python
+# هر تر کیبی از کاراکترهای الفبایی انگلیسی ، اعداد و آندرلاین به استثنای اینکه اولین کاراکتر نمی تواند عدد باشد
+# n1 = 0
+# _n1=0
+# n_1 = 0
+# 1n = 0 # error
+# name one = 0 # error
+# name-one = 0 # error
+
+
+# snake case
+name_one = "balalal"
+# camel case
+nameOne = "babababba"
+# big camel case
+NameOne = "balalalall"

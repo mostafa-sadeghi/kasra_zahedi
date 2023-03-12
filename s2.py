5c225acff38bd1b4bc360bc10e356d8247b4b78a
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-12 21:57:04 +0330

    sadeghi

---
+++ s2.py
@@ -0,0 +1,149 @@
+# multi-line str
+
+# message = "hello every body.\nwelcome to our python class.\nwe are going to learn everythings about python."
+# print(message)
+
+
+# message = """hello every body.
+# welcome to our python class.
+# we are going to learn everythings about python.
+# baalalallalallal
+# """
+# print(message)
+
+
+# message = '''hello every body.
+# welcome to our python class.
+# we are going to learn everythings about python.'''
+# print(message)
+
+
+# message = "he said : \"some thing\""
+# print(message)
+# message = '''he said : "some thing"'''
+# print(message)
+# message = 'he said : "some thing"'
+# print(message)
+
+# message = 'He said, "Aren\'t can\'t shouldn\'t wouldn\'t."'
+
+# print(message)
+# message = '''He said, "Aren't can't shouldn't wouldn't."'''
+# print(message)
+
+
+# name = "artin"
+# age = 12
+
+# message = "%s has  years old" % name
+
+# print(message)
+# message = "%s has %s years old" % (name,age)
+
+# print(message)
+
+
+# name = "artin"
+# age = 12
+
+# message = name + " has " + str(age) + " years old"
+
+# print(message)
+
+
+# name = "artin"
+# age = 12
+
+# message = f'{name} has {age} years old'
+
+# print(message)
+# print(name,"has",age,"years old")
+
+
+# print('blalalal ' * 10)
+
+# name = "nikan"
+
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+# name = input('enter a name:> ')
+
+# print(type(name))
+
+# برنامه ای بنویسید که نام، نام خانوادگی و سن دانشچویی را از ورودی دریافت نماید
+# و پیغامی مشابه زیر نمایش دهد
+# ali rezaei has 22 years old.
+
+
+# string = 'abcdefghi'
+
+# print(string[0:3])
+# cde
+# efgh
+# ghi
+# fgh
+# bc
+
+
+# shopping_list = []
+# print(type(shopping_list))
+
+# shopping_list.append("apple")
+# shopping_list.append("banana")
+# shopping_list.append("mashroom")
+
+# print("my shopping list", shopping_list)
+
+# how to remove an item from our list?
+# del shopping_list[1]
+# print("my shopping list", shopping_list)
+
+# shopping_list.remove("apple")
+# print("my shopping list", shopping_list)
+
+# shopping_list.insert(0, "something")
+# print("my shopping list", shopping_list)
+
+# shopping_list.pop()
+# print("my shopping list", shopping_list)
+
+# shopping_list.append("fries")
+# print("my shopping list", shopping_list)
+
+# result = shopping_list.pop(0)
+# print("my shopping list", shopping_list)
+# print("result value which removed:", result)
+
+
+# numbers = [1,2,3,4,5,6,7,2,3,4,2,2,2]
+# print("count of 2:", numbers.count(2))
+
+# numbers.extend(['a','b','c'])
+# print("my numbers", numbers)
+
+
+# numbers.append(['a','b','c'])
+# print("my numbers", numbers)
+
+
+# how to iterate over a list?
+
+# print(numbers[0])
+# print(numbers[1])
+# print(numbers[2])
+# numbers = [1,2,3,4,5,6,7,8,9]
+# print("odd numbers:",numbers[::2])
+# numbers = [1,2,3,4,5,6,7,8,9]
+# print("even numbers:",numbers[1::2])
+
+
+# exercise 1:
+# برنامه ای بنویسی که نام چهار دانشجو را از ورودی دریافت نماید
+# و در لیستی اضافه کند
+# در نهایت
+# اولید و آخرین دانش جو را نمایش دهد
+# سومین دانشجو را از لیست حذف نماید

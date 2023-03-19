bfd64bd7e49716a620ffafe6efa0a95bb69cc571
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-19 21:59:17 +0330

    sadeghi

---
+++ s3.py
@@ -0,0 +1,52 @@
+# string = "blalalal"
+# string2 = "aaaaaaa"
+
+# print(string + string2)
+
+# print(string * 4)
+
+# numbers = [1,2,3,4]
+# numbers2 = [5,6,7]
+# print(numbers + numbers2)
+# print(numbers * 4)
+
+
+# mylist = [-1, 0, 1, [3, 5], 'a']
+
+# print(mylist[3])
+# print(mylist[3][0])
+# print(mylist[3][1])
+
+# mylist[3][0] = 0
+# mylist[3][1] = 0
+
+# print(mylist)
+
+# del mylist[4]
+# print(mylist)
+
+# mylist.remove('a')
+# print(mylist)
+# mylist.pop()
+# print(mylist)
+
+
+names = ['ali', 'ramtin', 'reza', 'ramtin', 'cyrus', 'reza']
+print(names.count('ramtin'))
+
+# names2 = names.copy()
+# print(names2)
+# names.clear()
+# print(names2)
+# print(names)
+names.extend(['item1', '1'])
+names.insert(2, "maryam")
+names.reverse()
+names.sort()
+
+print('a' < 'b')
+print(ord('a'))
+print(ord('b'))
+print(ord('1'))
+
+print(names)

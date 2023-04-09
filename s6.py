85e6cc307be5ac0a818c78187afff4f7b0aabe9f
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-09 21:54:52 +0330

    sadeghi

---
+++ s6.py
@@ -0,0 +1,58 @@
+# x = range(5)
+# print(type(x))
+# print(x[0])
+# print(x[1])
+# print(x[2])
+# print(x[3])
+# print(x[-1])
+# print(x[4])
+# print(x[-5])
+
+# numbers = list(range(5))
+# print(numbers)
+# print(numbers[-1])
+# print(numbers[-2])
+# print(numbers[-3])
+# print(numbers[-4])
+# print(numbers[-5])
+
+# for i in range(5):
+#     print(i, end=' ')
+
+# for i in range(5):
+#     print("i want to learn web development...")
+#     print("something")
+# print()
+
+
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(5):
+#     print((i+1)*2)
+
+# for i in range(2,11,2):
+#     print(i)
+# for i in range(1,11,2):
+#     print(i)
+
+# for i in range(10,0,-1):
+#     print(i)
+# import math
+
+# for n in 'nikan':
+#     print(n)
+
+# for n in [2,4,5,9,0,-1]:
+#     print(n*3)
+
+# for n in (3,5,7,8,0):
+#     print(n**2)
+
+# for number in {4,9,16,32}:
+#     print(math.sqrt(number))
+
+

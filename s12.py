5e9a86495bc876723c2c623aa685b4fc5ee210a6
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-30 21:58:39 +0330

    sadeghi

---
+++ s12.py
@@ -0,0 +1,39 @@
+# plain_text = "This is a test. ABC abc"
+# for character in plain_text:
+#     # print(ord(character), end=" ")
+#     x = ord(character)
+#     x = x + 1
+#     result = chr(x)
+#     print(result, end="")
+# for character in 'Uijt!jt!b!uftu/!BCD!bcd':
+# print(ord(character), end=" ")
+# x = ord(character)
+# x = x - 1
+# result = chr(x)
+# print(result, end="")
+
+
+# def func(name):
+#     print("Hello",name)
+
+# func("ali")
+
+
+import tkinter as tk
+from tkinter import ttk
+
+
+def greet():
+    print("hello")
+
+# tkinter._test()
+
+
+root = tk.Tk()
+ttk.Label(root, text="سلام", background="red", anchor="center", padding=20, borderwidth=50, relief="sunken",
+          foreground="black", font=("B Titr Bold", 24, 'bold', 'underline')).pack()
+
+greet_button = ttk.Button(
+    root, text="Greet", cursor="hand2", padding=20, width=70, command=greet)
+greet_button.pack()
+root.mainloop()

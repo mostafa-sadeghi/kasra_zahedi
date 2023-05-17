8f0ead0b396ab98ca674bc6fe957d9334fff567b
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-17 22:00:42 +0330

    sadeghi

--- s15.py
+++ s15.py
@@ -48,17 +48,25 @@ my_variable = tk.StringVar()
 
 
 # my_variable.set(random.choice(GREETINGS))
+# root.columnconfigure(0, weight=1)
+# root.columnconfigure(1, weight=1)
+# root.rowconfigure(0, weight=1)
 
 # Text Widget
-text = tk.Text(root, height=8, width=12)
-text.insert("1.2","H")
-text.insert("1.0","Hello World")
-text.insert("1.3","aaaaa")
+# text = tk.Text(root, height=8, width=12)
+# text.insert("1.0","M")
+# text.insert("1.3","G")
+# text.insert("1.1","JJJJJJJJJJJJ")
+# text.insert("")
+# print(text.get("1.0","end"))
+# text.grid(row=0,column=0,sticky="e")
+# text_scroll = ttk.Scrollbar(root, orient="vertical", command=text.yview)
+# text_scroll.grid(row=0, column=1, sticky="nsw")
+# text["yscrollcommand"] = text_scroll.set
 
-text.pack()
 
-
-
+# اضافه کردن فریم
+# دو ویجت بالا را درون فریم قرار بده
 
 root.mainloop()
 
---
+++ s16.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+root = tk.Tk()
+# root.rowconfigure(0, weight=1)
+# root.columnconfigure(0, weight=1)
+
+# main_frame = ttk.Frame(root)
+# main_frame.grid(column=0, row=0, sticky="n")
+# main_frame.rowconfigure(0, weight=1, minsize=180)
+
+
+# tk.Label(main_frame, text="Number1").grid(column=0, row=0)
+# ttk.Separator(main_frame, orient="vertical").grid(
+#     column=1, row=0, sticky="nsew")
+# tk.Label(main_frame, text="Number2").grid(column=2, row=0)
+
+# def print_current_selection():
+#     print()
+
+image = Image.open("orangee.png")
+tk_image = ImageTk.PhotoImage(image)
+
+check_button = ttk.Checkbutton(
+    root, text="Pizza", image=tk_image, compound="right", command=lambda: print("hello"))
+check_button.pack()
+# check_button["state"] = "disabled"  # "normal"
+root.mainloop()
